a1a075aba00bdafed4082498043348b0f00b56ee
Esther Ugwueke <67696383+estherUgwueke@users.noreply.github.com>
2021-08-05 14:02:37 +0100

    Update stage_0_esther.py

--- stage_0_esther.py
+++ stage_0_esther.py
@@ -1,8 +1,24 @@
-Name = ("Esther Ugwueke")
+# Introduction _First assignment on HackBioInternship
+
+Name = "Esther Ugwueke"
 print(Name)
-Email = ("ugwuekeestherc@gmail.com")
+Email = "ugwuekeestherc@gmail.com"
 print(Email)
-Slack_Username = ("@Esther")
+Slack_Username = "@Esther"
 print(Slack_Username)
-Biostack = ("Data Science")
+Biostack = "Data Science"
 print(Biostack)
+Twitter = "@EstherUgwueke"
+print(Twitter)
+
+
+# Hamming distance
+
+def hamming_distance(Slack_Username, Twitter):
+    dist_counter = 0
+    for n in range(len(Slack_Username)):
+        if Slack_Username[n] != Twitter[n]:
+            dist_counter += 1
+    return dist_counter
+
+print(hamming_distance(Slack_Username, Twitter))
