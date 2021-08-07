64305cd170860ef853d301b63612c3b6f712bd59
Xenon <xzenon@gmail.com>
2021-08-07 22:36:30 +0300

    Added PHP script

---
+++ Yevgen_stage_0.php
@@ -0,0 +1,18 @@
+<?php
+
+function hamming($str1, $str2)
+{
+    $i = 0; $count = 0;
+    while (isset($str1[$i]) != '')
+    {
+        if ($str1[$i] != $str2[$i])
+            $count++;
+        $i++;
+    }
+    return $count;
+}
+ 
+$distance = hamming("Yevgen", "xzenon");    
+echo "Yevgen, xzenon@gmail.com, @Yevgen, Datascience, @xzenon,".$distance;
+
+?>
